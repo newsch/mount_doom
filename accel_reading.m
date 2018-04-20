4b9d9b0c047f07b89eb411c6216066393784476c
Evan Lloyd New-Schmidt <lloydns@outlook.com>
2018-04-20 17:32:42 -0400

    Add initial files
    The current state machine approach in mount_doom_speedrun is fairly
    successful but stops fairly quickly. Next steps would be to move to
    gradient reading and ascent.

---
+++ accel_reading.m
@@ -0,0 +1,14 @@
+accel_sub = rossubscriber('/accel');
+accel = zeros(3,1);
+pause(0.25)
+
+
+
+while true
+    accel = accel_sub.LatestMessage.Data;
+    x = accel(1);
+    y = accel(2);
+    z = accel(3);
+    disp("x: "+x+"  y: "+y+"  z: "+z)
+    pause(0.25);
+end
---
+++ calibration.m
@@ -0,0 +1,27 @@
+accel_sub = rossubscriber('/accel');
+pause(0.25)
+
+accel = accel_sub.LatestMessage.Data;
+
+x = accel(1);
+y = accel(2);
+z = accel(3);
+
+% linsolve
+% desired = [0 0 -1]';
+% R = linsolve(accel',desired');
+
+% rotations
+theta = tan(x/z);
+R = getR(0,-theta,0);
+
+corrected_a = R*accel;
+
+function R = getR(roll,pitch,yaw)
+    syms a b c
+    Rx = [1, 0, 0; 0, cos(a), -sin(a); 0, sin(a), cos(a)];
+    Ry = [cos(b), 0, sin(b); 0, 1, 0; -sin(b), 0, cos(b)];
+    Rz = [cos(c), -sin(c), 0; sin(c), cos(c), 0; 0, 0, 1];
+    Rsym = Rz*Ry*Rx;
+    R = double(subs(Rsym,[a b c],[roll pitch yaw]));
+end
